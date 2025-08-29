1530c8b7ec80a06250899df8ef6d9ea085ebf530
JamishaBade <badejamishagmail.com>
2025-08-29 08:46:03 -0400

    Framework of the project

---
+++ app/_components/Header.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Image from 'next/image'
+import { Http2ServerRequest } from 'http2'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+const menuOptions=[
+{
+  name:'Home',
+  path:'/'
+},
+{name:'Pricing', 
+  path:'/path'},
+{
+  name:'Contact us',
+  path: '/contact-us',
+}
+
+]
+
+function Header() {
+  return (
+    <div className='flex justify-between item-center p-4'>
+      <div className="flex gap-3">
+        {/* Logo */}
+        <Image src="/logo.svg" alt="logo" width={30} height={30} />
+        {/* App name */}
+        <h2 className="font-bold text-2xl">ZourneyAI</h2>
+      </div>
+
+      <div className='flex gap-5 items-center'>
+        {menuOptions.map((menu, index) => (
+        <Link key={index} href={menu.path}>
+        <h2 className="text-lg transition hover:scale-105 hover:text-[var(--primary)]">
+        {menu.name}
+        </h2>          
+        </Link>
+        ))}
+      </div>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
+
+
+export default Header
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="mt-24 flex items-center flex justify-center">
+        {/* Content */}
+        <div className="max-w-3xl w-full text-center">
+          <h1 className='text-xl md:text-5xl font-bold'>Hey, I'm your personal Trip Planner <span className="text-primary"> ZourneyAI.</span></h1>
+          <p>Tell me what you want, I am thrilled to help you out.</p>
+        </div>
+        {/* Input Box */}
+
+        {/* Suggestion list */}
+
+        {/* Video Section */}
+
+    </div>
+  )
+}
+
+export default Hero
