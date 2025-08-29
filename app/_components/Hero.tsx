2a88442d4c8887691eeccabb9bfe22fb4e0ea7fc
JamishaBade <badejamishagmail.com>
2025-08-29 10:55:19 -0400

    UI update
    added new interfaces

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Send } from "lucide-react"
+import { CompassIcon, Divide, Globe2, Package2Icon, PiggyBankIcon, PlaneIcon, Send } from "lucide-react";
+import { Purple_Purse } from "next/font/google";
 
+const suggestions = [
+  {
+    title: "Create New Trip",
+    icon: <Globe2 className="text-blue-400 h-5 w-5" />,
+  },
+  {
+    title: "Discover your true destination",
+    icon: <PlaneIcon className="text-green-400 h-5 w-5" />,
+  },
+  {
+    title: "Plan. Explore. Go",
+    icon: <PiggyBankIcon className="text-pink-400 h-5 w-5" />,
+  },
+  {
+    title: "Turn your dreams into reality with ZourneyAI",
+    icon: <CompassIcon className="text-yellow-400 h-5 w-5" />,
+  },
+];
 
 function Hero() {
   return (
@@ -18,24 +37,32 @@ function Hero() {
         </p>
       </div>
       {/* Input Box */}
-     <div className="w-full max-w-3xl">
-  <div className="relative border border-gray-300 rounded-2xl p-4 shadow-2xl bg-white">
-    {/* Textarea */}
-    <textarea
-      className="w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none pr-16"
-      placeholder="Create a trip from Paris to New York City."
-    />
-    {/* Button */}
-    <div className="absolute bottom-4 right-4">
-      <Button size="icon" className="bg-[var(--primary)] text-white">
-        <Send />
-      </Button>
-    </div>
-  </div>
-</div>
-
+      <div className="w-full max-w-3xl">
+        <div className="relative border border-gray-300 rounded-2xl p-4 shadow-2xl bg-white">
+          {/* Textarea */}
+          <textarea
+            className="w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none pr-16"
+            placeholder="Create a trip from Paris to New York City."
+          />
+          {/* Button */}
+          <div className="absolute bottom-4 right-4">
+            <Button size="icon" className="bg-[var(--primary)] text-white">
+              <Send />
+            </Button>
+          </div>
+        </div>
+      </div>
 
       {/* Suggestion list */}
+
+      <div className='flex gap-6'>
+        {suggestions.map((suggestions, index) => (
+          <div key={index} className='flex items-center gap-2 border border-gray-100 rounded-full p-2 hover:bg-[var(--primary)] hover:text-white'>
+            {suggestions.icon}
+            <h2>{suggestions.title}</h2>
+          </div>
+        ))}
+      </div>
 
       {/* Video Section */}
     </div>
