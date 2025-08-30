eb8162fd4b7b4781e3c88e6e5b49956fd2eab7c0
JamishaBade <badejamishagmail.com>
2025-08-29 21:31:26 -0400

    added a globe through magic ui

---
+++ app/_components/GlobeObject.tsx
@@ -0,0 +1,6 @@
+import { Globe } from "@/components/magicui/globe"
+
+function GlobeObj(){
+  return (<Globe className="relative w-full h-[200px] w-[200px] overflow-hidden"/>)
+}
+export default GlobeObj;
